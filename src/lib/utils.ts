c281d726a2297c27c9512651aee97ef5cce4c76c
nickapplicable <numbereft@gmail.com>
2024-04-11 18:45:47 +1200

    add vscode settings and oslo-based generateId function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { generateRandomString, alphabet } from 'oslo/crypto'
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
+}
+
+export const generateId = (length = 15) => {
+	return generateRandomString(length, alphabet('a-z', '0-9'))
 }
