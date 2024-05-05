2507783c0ce99943f2b8d3a8dfdee59736f0cc2b
nickapplicable <numbereft@gmail.com>
2024-05-05 17:57:23 +1200

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,16 @@ import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [tsconfigPaths(), react()],
+	server: {
+		port: 3001,
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
 		},
+	},
+	build: {
+		emptyOutDir: true,
+		outDir: '../server/public',
 	},
 })
